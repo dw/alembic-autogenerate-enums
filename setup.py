80f2cbd2bbe4c4de8ceedac4929a42dcbc7e6380
David Wilson <dw@botanicus.net>
2017-02-06 14:45:47 +0000

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='alembic_autogenerate_enums',
-    description='''
-        Alembic hook that causes --autogenerate to output PostgreSQL ALTER TYPE
-        statements.
-    ''',
-    version='0.0.1',
+    description=(
+        "Alembic hook that causes --autogenerate to output PostgreSQL ALTER "
+        "TYPE statements."
+    ),
+    version='0.0.2',
     author='David Wilson',
     url='http://github.com/dw/alembic-autogenerate-enums/',
     py_modules=['alembic_autogenerate_enums'],
