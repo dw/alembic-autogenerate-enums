1735fd6d663ef85896e207809cef58e0842b67bb
Pierce Freeman <piercefreeman@gmail.com>
2023-03-30 15:36:52 -0700

    Bump minor version to 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "Alembic hook that causes --autogenerate to output PostgreSQL ALTER "
         "TYPE statements."
     ),
-    version='0.0.2',
+    version='0.1.0',
     author='David Wilson',
     url='http://github.com/dw/alembic-autogenerate-enums/',
     py_modules=['alembic_autogenerate_enums'],
